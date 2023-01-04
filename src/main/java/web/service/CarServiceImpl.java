f5d1ed7158630c342703136868c0ccf69f70c821
MarinaKhristosova <108974887+MarinaKhristosova@users.noreply.github.com>
2023-01-04 21:52:17 +0300

    Update CarServiceImpl.java

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,18 +5,21 @@ import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class CarServiceImpl extends CarService {
 
     public List<Car> getCarList(int count) {
-        List <Car> cars = new ArrayList();
+        List<Car> cars = new ArrayList();
         cars.add(new Car("Volvo", "x90", 2020));
         cars.add(new Car("Toyota", "Corolla", 2018));
         cars.add(new Car("Toyota", "Camry", 2022));
         cars.add(new Car("Mercedes", "Gelendvagen", 2011));
         cars.add(new Car("Fiat", "Bravo", 2004));
+        if (count >= 5 || count <= 0) {
+            return cars;
+        }
 
         return cars.stream().limit(count).toList();
     }
 }
-
